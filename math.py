3fdf4aee560e1cf09820f45fcb2101c5f9f3d24f
SushmithaPurem <72455535+SushmithaPurem@users.noreply.github.com>
2022-03-07 14:19:43 +0530

    intial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#Add implementation
+ def add(x,y):
+	pass
+#subtract implementation   
+def subtract(x,y):
+	return x-y          #on master branch
+#Multiply implementation
+def multiply(x,y):
+	return x*y     #on bug456 branch
+#Divide implementation
+def divide(x,y):
+
+	if y==0:
+        return DIVIDE_BY_0
+    else :
+        return x/y        #on master baranch
+#square implementation
+def square(x):
+    return x^2
+    
